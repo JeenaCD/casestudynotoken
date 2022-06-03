0a0e87a3a48a3c1598efef9b957d776508a4f430
JeenaCD <102554603+JeenaCD@users.noreply.github.com>
2022-06-04 00:04:33 +0530

    new

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/Library',{useNewUrlParser: true,useUnifiedTopology: true});
-// mongoose.connect('mongodb://127.0.0.1:27017/Library',{useNewUrlParser: true,useUnifiedTopology: true});
 const dotenv = require('dotenv');
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true,useUnifiedTopology: true});
